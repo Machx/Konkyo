cb30120cabdd0be24dee7cb39e078c5475e12cef
Colin Wheeler <cocoasamurai@gmail.com>
2019-11-30 08:55:13 -0800

    change the unit test to match the api

--- Tests/KonkyoTests/KonkyoTests.swift
+++ Tests/KonkyoTests/KonkyoTests.swift
@@ -6,7 +6,7 @@ final class KonkyoTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Konkyo().text, "Hello, World!")
+        XCTAssertEqual(Konkyo().version, 1)
     }
 
     static var allTests = [
