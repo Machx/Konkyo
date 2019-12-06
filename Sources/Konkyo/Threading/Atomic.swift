722a02a35764b1bb3ca0462a3fc3453cb7559dc5
Colin Wheeler <cocoasamurai@gmail.com>
2019-12-05 22:55:14 -0800

    implement new atomic type

--- Sources/Konkyo/Threading/Atomic.swift
+++ Sources/Konkyo/Threading/Atomic.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public final class Atomic<Value> {
+	private var _value: Value
+	private let queue = DispatchQueue(label: "com.Konkyo.Atomic.\(String(describing: Value))")
+	
+	public var value: Value {
+		get {
+			return queue.sync { self._value }
+		}
+	}
+	
+	public func mutate(_ transform: (inout Value) -> Void) {
+		queue.sync {
+			transform(&self._value)
+		}
+	}
+}
