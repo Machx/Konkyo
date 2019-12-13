789d931ab65b47f0e9da3035033e9a7c72d32d72
Colin Wheeler <cocoasamurai@gmail.com>
2019-12-12 23:31:47 -0800

    setup operation for testing

--- Tests/KonkyoTests/KonkyoTests.swift
+++ Tests/KonkyoTests/KonkyoTests.swift
@@ -1,6 +1,26 @@
 import XCTest
 @testable import Konkyo
 
+public class IncrementOp: AsynchronousOperationBase {
+	let queue = DispatchQueue(label: "com.thing")
+	
+	var value: Int = 0
+	
+	public init(value: Int) {
+		super.init()
+		self.value = value
+	}
+	
+	public override func start() {
+		isExecuting = true
+		queue.async { [weak self] in
+			guard let self = self else { return }
+			self.value += 1
+			self.isFinished = true
+		}
+	}
+}
+
 @available(OSX 10.12, *)
 final class KonkyoTests: XCTestCase {
 	
