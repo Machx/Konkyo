575743b80ea6b1e5a7f1f58c5c9a2e24ac3d3191
Colin Wheeler <cocoasamurai@gmail.com>
2019-12-28 18:26:46 -0600

    add atomic tests in their own file

--- Tests/KonkyoTests/AtomicTests.swift
+++ Tests/KonkyoTests/AtomicTests.swift
@@ -7,12 +7,27 @@
 
 import Foundation
 import XCTest
+@testable import Konkyo
 
 @available(OSX 10.12, *)
 final class AtomicTests: XCTestCase {
 	
 	func testAtomic() {
-		XCTAssertEqual(3, 3)
+		let numbers = Atomic<[Int]>([])
+		let queue = DispatchQueue(label: "com.konkyo.unittests", attributes: [.concurrent])
+		let group = DispatchGroup()
+		
+		for i in 1...10000 {
+			queue.async(group:group) {
+				numbers.mutate { (numbers) in
+					numbers.append(i)
+				}
+			}
+		}
+		
+		group.wait()
+		
+		XCTAssertEqual(numbers.value.count, 10000)
 	}
 	
 	static var allTests = [
