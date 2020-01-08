43485401b24d9c9a81af8005bd82fdfb1cd176a8
Colin Wheeler <cocoasamurai@gmail.com>
2020-01-07 23:36:34 -0800

    remove operation tests from konkyo tests

--- Tests/KonkyoTests/KonkyoTests.swift
+++ Tests/KonkyoTests/KonkyoTests.swift
@@ -16,25 +16,7 @@
 import XCTest
 @testable import Konkyo
 
-public class IncrementOp: AsynchronousOperationBase {
-	let queue = DispatchQueue(label: "com.thing")
-	
-	var value: Int = 0
-	
-	public init(value: Int) {
-		super.init()
-		self.value = value
-	}
-	
-	public override func start() {
-		isExecuting = true
-		queue.async { [weak self] in
-			guard let self = self else { return }
-			self.value += 1
-			self.isFinished = true
-		}
-	}
-}
+
 
 @available(OSX 10.12, *)
 final class KonkyoTests: XCTestCase {
@@ -46,17 +28,7 @@ final class KonkyoTests: XCTestCase {
 		XCTAssertEqual(version.bugfix, 0)
     }
 	
-	/// Test Operation Access on isExecuting isFinished in AsyncOperationBase
-	func testOperation() {
-		let queue = OperationQueue()
-		queue.maxConcurrentOperationCount = 1
-		
-		let op = IncrementOp(value: 1)
-		queue.addOperation(op)
-		
-		queue.waitUntilAllOperationsAreFinished()
-		XCTAssertEqual(op.value, 2)
-	}
+	
 
     static var allTests = [
         ("testVersion", testVersion),
