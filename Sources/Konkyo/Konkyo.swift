76c2f78f1406f1d5f4cabdb71021c1f479aefe60
Colin Wheeler <cocoasamurai@gmail.com>
2020-01-11 20:19:09 -0800

    update to 2020

--- Sources/Konkyo/Konkyo.swift
+++ Sources/Konkyo/Konkyo.swift
@@ -1,5 +1,5 @@
 ///
-/// Copyright 2019 Colin Wheeler
+/// Copyright 2020 Colin Wheeler
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
