50a6c115b2c2e45d6b4dca6e6c6143eda43d8ca0
Colin Wheeler <cocoasamurai@gmail.com>
2020-02-25 20:51:34 -0800

    add initial mutex test

--- Tests/KonkyoTests/MutexTests.swift
+++ Tests/KonkyoTests/MutexTests.swift
@@ -5,4 +5,26 @@
 //  Created by Colin Wheeler on 2/25/20.
 //
 
+import XCTest
 import Foundation
+import Konkyo
+
+final class MutexTests: XCTestCase {
+	
+	func testMutex() {
+		let mutex = Mutex()
+		var total = 0
+		DispatchQueue.concurrentPerform(iterations: 10000) { (index) in
+			mutex.lock()
+			total += index
+			mutex.unlock()
+		}
+		
+		XCTAssertEqual(total, 49995000)
+	}
+	
+	
+    static var allTests = [
+        ("testMutex", testMutex),
+    ]
+}
