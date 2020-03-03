9687b59e513631abd49cb6f730838b4985f584d9
Colin Wheeler <cocoasamurai@gmail.com>
2020-03-02 22:34:56 -0800

    add unfair lock test

--- Tests/KonkyoTests/UnfairLockTests.swift
+++ Tests/KonkyoTests/UnfairLockTests.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import XCTest
+import Konkyo
+
+@available(OSX 10.12, *)
+final class UnfairLockTests: XCTestCase {
+	
+	func testUnfairLock() {
+		let lock = UnfairLock()
+		var total = 0
+		let iterations = Int.random(in: 5000...15000)
+		DispatchQueue.concurrentPerform(iterations: iterations) { (index) in
+			lock.lock()
+			total += index + 1 // Adjust because indexes start at 0.
+			lock.unlock()
+		}
+		
+		let expected = (iterations * (iterations + 1)) / 2
+		
+		XCTAssertEqual(total, expected, "Failed with Iterations: \(iterations)")
+	}
+	
+	static var allTests = [
+        ("testUnfairLock", testUnfairLock),
+    ]
+}
