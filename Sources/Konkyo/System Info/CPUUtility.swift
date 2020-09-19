5b1394353303af4d0ff55b0f00bd2ff93d178aa1
Colin Wheeler <cocoasamurai@gmail.com>
2020-09-18 17:07:37 -0700

    transfer the cpu utility code

--- Sources/Konkyo/System Info/CPUUtility.swift
+++ Sources/Konkyo/System Info/CPUUtility.swift
@@ -1,8 +1,30 @@
-//
-//  File.swift
-//  
-//
-//  Created by Colin Wheeler on 9/18/20.
-//
+///
+/// Copyright 2020 Colin Wheeler
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
 
 import Foundation
+import Darwin.sys.sysctl
+
+final class CPUInfo {
+	/// Returns the number of CPU Cores on Device
+	///
+	/// - returns: Number of CPU Cores on Device
+	class func cpuCoreCount() -> Int {
+		var size:Int = 0
+		sysctlbyname("hw.ncpu", nil, &size, nil, 0)
+		var ncpu:Int = 0
+		sysctlbyname("hw.ncpu", &ncpu, &size, nil, 0)
+		return ncpu
+	}
+}
