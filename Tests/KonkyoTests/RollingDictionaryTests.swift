276512400b971b0c986e6dd3a253c1f758622c6c
Colin Wheeler <cocoasamurai@gmail.com>
2021-02-26 15:23:27 -0800

    first test

--- Tests/KonkyoTests/RollingDictionaryTests.swift
+++ Tests/KonkyoTests/RollingDictionaryTests.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import XCTest
+import Konkyo
+
+final class RollingDictionaryTests: XCTestCase {
+	
+	func testBasicRollingDictionary() {
+		var dictionary = RollingDictionary<String,Int>(limit: 1)
+		
+		dictionary["Ted"] = 1
+		
+		let keys1 = dictionary.keys
+		
+		XCTAssertEqual(keys1, 1)
+		XCTAssertEqual(["Ted"], keys1)
+		
+		dictionary["Lasso"] = 2
+	}
+}
