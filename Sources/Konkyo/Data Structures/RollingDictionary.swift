d2ac46b9ee6f51fc62dc2163b5757cf071eb172a
Colin Wheeler <cocoasamurai@gmail.com>
2021-02-28 21:37:16 -0800

    formatting change to be more compact

--- Sources/Konkyo/Data Structures/RollingDictionary.swift
+++ Sources/Konkyo/Data Structures/RollingDictionary.swift
@@ -53,9 +53,9 @@ public struct RollingDictionary<Key:Hashable,Value> {
 		}
 	}
 	
-	public var keys: Dictionary<Key, Value>.Keys {
-		return _dictionary.keys
-	}
+	public var keys: Dictionary<Key, Value>.Keys { _dictionary.keys }
+	
+	public func getKeyLimit() -> Int { _limit }
 	
 	public mutating func setLimit(_ maxKeysCount: Int) {
 		_limit = maxKeysCount
