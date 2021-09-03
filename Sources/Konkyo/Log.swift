c0abbc1e39ae44316312674626cec19686627021
Colin Wheeler <cocoasamurai@gmail.com>
2021-09-03 11:42:18 -0700

    add cgrect convenience

--- Sources/Konkyo/Log.swift
+++ Sources/Konkyo/Log.swift
@@ -52,3 +52,15 @@ extension CGPoint: CustomStringConvertible {
 		"CGPoint { x: \(String(format: "%.2f", x)) y: \(String(format: "%.2f", y))"
 	}
 }
+
+extension CGRect: CustomStringConvertible {
+	/// Returns a string representation of the rect made by rounding its x,y,width and height to 2 decimal places.
+	public var description: String {
+		"""
+		CGRect {
+			Origin: CGPoint { x: \(String(format: "%.2f", origin.x)) y: \(String(format: "%.2f", origin.y)) }
+			  Size: CGSize { width: \(String(format: "%.2f", size.width)) height: \(String(format: "%.2f", size.height)) }
+		}
+		"""
+	}
+}
