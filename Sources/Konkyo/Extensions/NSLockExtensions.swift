ef0d26aa8cb1bd851ff7b91d496fbd09e1ee203b
Colin Wheeler <cocoasamurai@gmail.com>
2022-10-14 11:40:31 -0700

    add withlock

--- Sources/Konkyo/Extensions/NSLockExtensions.swift
+++ Sources/Konkyo/Extensions/NSLockExtensions.swift
@@ -1,8 +1,17 @@
 //
-//  File.swift
+//  NSLockExtensions.swift
 //  
 //
 //  Created by Colin Wheeler on 10/14/22.
 //
 
 import Foundation
+
+public extension NSLock {
+
+	func withLock(_ block: ()->Void) {
+		self.lock()
+		block()
+		self.unlock()
+	}
+}
