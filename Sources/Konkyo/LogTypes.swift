058f75f8b77a53b7073626532c6b45e04d26cb60
Colin Wheeler <cocoasamurai@gmail.com>
2023-07-29 23:45:32 -0700

    move them to the new file
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Sources/Konkyo/LogTypes.swift
+++ Sources/Konkyo/LogTypes.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+extension CGPoint: CustomStringConvertible {
+	/// Returns a string representation of the point made by rounding to 2 decimal places.
+	public var description: String {
+		"CGPoint { x: \(String(format: "%.2f", x)) y: \(String(format: "%.2f", y))"
+	}
+}
+
+extension CGRect: CustomStringConvertible {
+	/// Returns a string representation of the rect made by rounding its x,y,width and height to 2 decimal places.
+	public var description: String {
+		"""
+		CGRect {
+			Origin: CGPoint { x: \(String(format: "%.2f", origin.x)) y: \(String(format: "%.2f", origin.y)) }
+			  Size: CGSize { width: \(String(format: "%.2f", size.width)) height: \(String(format: "%.2f", size.height)) }
+		}
+		"""
+	}
+}
