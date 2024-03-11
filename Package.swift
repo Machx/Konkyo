577aa95c023b2f2ee01f19d41b9fc82e1f1ed59f
Colin Wheeler <cocoasamurai@gmail.com>
2024-03-10 21:55:44 -0700

    concurrency checking
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Package.swift
+++ Package.swift
@@ -22,9 +22,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Konkyo",
-            dependencies: []),
+		.target(
+			name: "Konkyo",
+			dependencies: [],
+			swiftSettings: [
+				.enableExperimentalFeature("StrictConcurrency")
+			]),
         .testTarget(
             name: "KonkyoTests",
             dependencies: ["Konkyo"]),
