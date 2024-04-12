04e0d4c15507dcfb17a581e561062d61ce76275b
Colin Wheeler <cocoasamurai@gmail.com>
2024-04-12 13:41:47 -0700

    add settings
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Sources/Konkyo/PropertyWrappers/Settings.swift
+++ Sources/Konkyo/PropertyWrappers/Settings.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+@propertyWrapper public struct Preferences<Value: Codable> {
+	public let key: String
+	public let defaultValue: Value
+
+	public init(key: String, defaultValue: Value) {
+		self.key = key
+		self.defaultValue = defaultValue
+	}
+
+	public var wrappedValue: Value {
+		get { UserDefaults.standard.object(forKey: key) as? Value ?? defaultValue }
+		set { UserDefaults.standard.setValue(newValue, forKey: key) }
+	}
+}
