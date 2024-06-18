b38d4d06bd02db73b1f7e333377edc29ccf945b5
Colin Wheeler <cocoasamurai@gmail.com>
2024-06-17 23:06:09 -0700

    bump mac version
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Konkyo",
 	platforms: [
-		.macOS(.v13),
 		.iOS(.v16),
+		.macOS(.v14),
 	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
