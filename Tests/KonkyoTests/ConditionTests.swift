ad050d3be9c227c4d24a41d72c54eba9d620b08f
Colin Wheeler <cocoasamurai@gmail.com>
2024-09-28 23:33:25 -0700

    add back
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/KonkyoTests/ConditionTests.swift
+++ Tests/KonkyoTests/ConditionTests.swift
@@ -58,6 +58,21 @@ func testWaitUntilDate() async throws {
 	#expect(result == true)
 }
 
+func testWaitUntilDate2() {
+	let condition = Condition()
+	let expectation = XCTestExpectation()
+	expectation.isInverted = true
+
+	DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 1.0) {
+		condition.signal()
+		expectation.fulfill()
+	}
+
+	// Give it a date way in the past so it should reject it
+	let result = condition.wait(until: Date(timeIntervalSince1970: 6))
+	XCTAssertFalse(result)
+}
+
 final class CWConditionTests: XCTestCase {
 	
 	func testWaitUntilDateNegative() {
