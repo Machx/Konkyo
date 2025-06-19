29df094d6020a9416e8cb92fbb72f3e5d032d3d3
Colin Wheeler <cocoasamurai@gmail.com>
2025-06-18 23:49:17 -0700

    add logging kit as a dep
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Package.swift
+++ Package.swift
@@ -17,14 +17,16 @@ let package = Package(
     ],
     dependencies: [
         // will update versions later, for now just specify branch
-		.package(url: "https://github.com/Machx/LoggingKit.git", .branch("main"))
+		.package(url: "https://github.com/Machx/LoggingKit.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
 			name: "Konkyo",
-			dependencies: [],
+			dependencies: [
+				.product(name: "LoggingKit", package: "LoggingKit")
+			],
 			swiftSettings: [
 				//.enableExperimentalFeature("StrictConcurrency")
 			]),
